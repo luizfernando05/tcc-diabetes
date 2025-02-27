4faf7e9f06b92dfcb1fe5105962169d978a6953d
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-02-27 13:41:38 -0300

    :sparkles: feat(api-backend): add Admin entity

---
+++ root/api/src/domain/entities/Admin.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('admins')
+export class Admin {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default Admin;
