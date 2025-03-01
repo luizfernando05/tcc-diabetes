54f7b49cd1c7985560873f1500e7cc0a99643712
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-01 13:49:28 -0300

    :sparkles: feat(api-backend): add CreateAdminUseCase for admin creation logic and include bcryptjs for password hashing

---
+++ root/api/src/application/Admin/use-cases/CreateAdminUseCase.ts
@@ -0,0 +1,28 @@
+import { IAdminRepository } from '@domain/repositories/IAdminRepository';
+import { CreateAdminDTO } from '../dtos/CreateAdminDto';
+import Admin from '@domain/entities/Admin';
+import { AppError } from '@presentation/errors/AppError';
+import { hash } from 'bcryptjs';
+
+export class CreateAdminUseCase {
+  constructor(private adminRepository: IAdminRepository) {}
+
+  async execute(data: CreateAdminDTO): Promise<Admin> {
+    const existingAdmin = await this.adminRepository.findByEmail(data.email);
+
+    if (existingAdmin) {
+      throw new AppError('Email already in use.', 409);
+    }
+
+    const hashedPassword = await hash(data.password, 10);
+
+    const admin = new Admin();
+    admin.name = data.name;
+    admin.email = data.email;
+    admin.password = hashedPassword;
+
+    return this.adminRepository.create(admin);
+  }
+}
+
+export default CreateAdminUseCase;
