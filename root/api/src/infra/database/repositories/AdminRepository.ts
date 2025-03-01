c16f078d36c6d5f9b80c425491a59ddd48ff30d9
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-01 13:35:26 -0300

    :sparkles: feat(api-backend): implement AdminRepository for admin management

---
+++ root/api/src/infra/database/repositories/AdminRepository.ts
@@ -0,0 +1,19 @@
+import Admin from '@domain/entities/Admin';
+import { IAdminRepository } from '@domain/repositories/IAdminRepository';
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../data-source';
+
+export class AdminRepository implements IAdminRepository {
+  private ormRepository: Repository<Admin>;
+
+  constructor() {
+    this.ormRepository = AppDataSource.getRepository(Admin);
+  }
+
+  async create(admin: Admin): Promise<Admin> {
+    const createAdmin = this.ormRepository.create(admin);
+    return this.ormRepository.save(createAdmin);
+  }
+}
+
+export default AdminRepository;
