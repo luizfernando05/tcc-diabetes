dd7d421b56b056464eab8632e6feae7778c374c9
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-01 13:17:16 -0300

    :sparkles: feat(api-backend): add relationship between Prediction and Patient entities

--- root/api/src/domain/entities/Prediction.ts
+++ root/api/src/domain/entities/Prediction.ts
@@ -2,13 +2,20 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import Patient from './Patient';
 
 @Entity('predictions')
 export class Prediction {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @ManyToOne(() => Patient, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'patient_id' })
+  patient: Patient;
 
   @Column({ name: 'prediction_result', type: 'varchar' })
   predictionResult: string;
