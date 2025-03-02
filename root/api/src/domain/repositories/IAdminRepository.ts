00a6a49a6f55ff3928762ec0fe2aff5a7fbb6294
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-02 11:46:08 -0300

    :sparkles: feat(admin): add findById method to IAdminRepository for retrieving admin by ID

--- root/api/src/domain/repositories/IAdminRepository.ts
+++ root/api/src/domain/repositories/IAdminRepository.ts
@@ -3,5 +3,6 @@ import Admin from '@domain/entities/Admin';
 export interface IAdminRepository {
   create(admin: Admin): Promise<Admin>;
   findByEmail(email: string): Promise<Admin | null>;
+  findById(id: string): Promise<Admin | null>;
   update(admin: Admin): Promise<Admin>;
 }
