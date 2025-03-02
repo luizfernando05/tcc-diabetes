33ade2ce5a9be515ec742e85f4d177a64113d7e4
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-02 12:57:31 -0300

    :sparkles: feat(admin): add route for updating admin details

--- root/api/src/routes/Admin/admin.routes.ts
+++ root/api/src/routes/Admin/admin.routes.ts
@@ -1,10 +1,12 @@
 import CreateAdminController from '@infra/http/controllers/Admin/CreateAdminController';
 import { LoginAdminController } from '@infra/http/controllers/Admin/LoginAdminController';
+import { UpdateAdminController } from '@infra/http/controllers/Admin/UpdateAdminController';
 import { Router } from 'express';
 
 const adminRoutes = Router();
 const createAdminController = new CreateAdminController();
 const loginAdminController = new LoginAdminController();
+const updateAdminController = new UpdateAdminController();
 
 adminRoutes.post('/', (req, res, next) => {
   createAdminController.handle(req, res, next);
@@ -12,6 +14,10 @@ adminRoutes.post('/', (req, res, next) => {
 
 adminRoutes.post('/login', (req, res, next) => {
   loginAdminController.handle(req, res, next);
+});
+
+adminRoutes.put('/:id', (req, res, next) => {
+  updateAdminController.handle(req, res, next);
 });
 
 export { adminRoutes };
