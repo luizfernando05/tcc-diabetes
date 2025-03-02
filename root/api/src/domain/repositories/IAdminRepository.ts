10c2b84b2e60669ddad9e7b74d3729b407becf83
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-02 11:40:25 -0300

    :sparkles: feat(admin): add update method to IAdminRepository for updating admin details

--- root/api/src/domain/repositories/IAdminRepository.ts
+++ root/api/src/domain/repositories/IAdminRepository.ts
@@ -3,4 +3,5 @@ import Admin from '@domain/entities/Admin';
 export interface IAdminRepository {
   create(admin: Admin): Promise<Admin>;
   findByEmail(email: string): Promise<Admin | null>;
+  update(admin: Admin): Promise<Admin>;
 }
