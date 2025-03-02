802ff919111e5f5b54cd36f934f02f8256afc36c
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-02 11:09:58 -0300

    :sparkles: feat(admin): implement LoginAdminUseCase for admin authentication

---
+++ root/api/src/application/Admin/use-cases/LoginAdminUseCase.ts
@@ -0,0 +1,37 @@
+import AdminRepository from '@infra/database/repositories/AdminRepository';
+import { LoginAdminDTO } from '../dtos/LoginAdminDTO';
+import { AppError } from '@presentation/errors/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+export class LoginAdminUseCase {
+  constructor(private adminRepository: AdminRepository) {}
+
+  async execute({ email, password }: LoginAdminDTO): Promise<string> {
+    const admin = await this.adminRepository.findByEmail(email);
+
+    if (!admin) {
+      throw new AppError('Invalid credentials.', 401);
+    }
+
+    const passwordCheck = await compare(password, admin.password);
+
+    if (!passwordCheck) {
+      throw new AppError('Wrong password.', 401);
+    }
+
+    if (!process.env.JWT_SECRET) {
+      throw new AppError('JWT secret is not defined.', 500);
+    }
+
+    const token = sign({ id: admin.id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+
+    console.log(token);
+
+    return token;
+  }
+}
+
+export default LoginAdminUseCase;
