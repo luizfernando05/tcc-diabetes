a2e9cd7b00583844073dae41b7e316175f678a83
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-02 11:21:25 -0300

    :sparkles: feat(admin): add login route to admin routes for handling admin login requests

--- root/api/src/routes/Admin/admin.routes.ts
+++ root/api/src/routes/Admin/admin.routes.ts
@@ -1,11 +1,17 @@
 import CreateAdminController from '@infra/http/controllers/Admin/CreateAdminController';
+import { LoginAdminController } from '@infra/http/controllers/Admin/LoginAdminController';
 import { Router } from 'express';
 
 const adminRoutes = Router();
 const createAdminController = new CreateAdminController();
+const loginAdminController = new LoginAdminController();
 
 adminRoutes.post('/', (req, res, next) => {
   createAdminController.handle(req, res, next);
+});
+
+adminRoutes.post('/login', (req, res, next) => {
+  loginAdminController.handle(req, res, next);
 });
 
 export { adminRoutes };
