cac3376157cf78d63fd01e4395db548145e399c5
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-03 13:31:15 -0300

    :sparkles: feat(admin): add findAll method to IAdminRepository for retrieving all admins

--- root/api/src/domain/repositories/IAdminRepository.ts
+++ root/api/src/domain/repositories/IAdminRepository.ts
@@ -4,5 +4,6 @@ export interface IAdminRepository {
   create(admin: Admin): Promise<Admin>;
   findByEmail(email: string): Promise<Admin | null>;
   findById(id: string): Promise<Admin | null>;
+  findAll(): Promise<Admin[]>;
   update(admin: Admin): Promise<Admin>;
 }
