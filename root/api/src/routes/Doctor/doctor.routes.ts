2d4dda9b536ab79928d50bf1b4e9e724bae26358
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-05 13:29:01 -0300

    :sparkles: feat(api-backend): add login route for doctor authentication

--- root/api/src/routes/Doctor/doctor.routes.ts
+++ root/api/src/routes/Doctor/doctor.routes.ts
@@ -1,11 +1,17 @@
 import CreateDoctorController from '@infra/http/controllers/Doctor/CreateDoctorController';
+import LoginDoctorController from '@infra/http/controllers/Doctor/LoginDoctorController';
 import { Router } from 'express';
 
 const doctorRoutes = Router();
 const createDoctorController = new CreateDoctorController();
+const loginDoctorController = new LoginDoctorController();
 
 doctorRoutes.post('/', (req, res, next) => {
   createDoctorController.handle(req, res, next);
+});
+
+doctorRoutes.post('/login', (req, res, next) => {
+  loginDoctorController.handle(req, res, next);
 });
 
 export { doctorRoutes };
