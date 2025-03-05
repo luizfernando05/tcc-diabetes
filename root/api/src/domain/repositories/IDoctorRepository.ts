55fc28bb96ff1921eba04132c245b5caa85a1b5e
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-05 11:03:06 -0300

    :sparkles: feat(api-backend): add update method to IDoctorRepository for updating doctor information

--- root/api/src/domain/repositories/IDoctorRepository.ts
+++ root/api/src/domain/repositories/IDoctorRepository.ts
@@ -5,4 +5,5 @@ export interface IDoctorRepository {
   findByEmail(email: string): Promise<Doctor | null>;
   findAll(): Promise<Doctor[]>;
   findById(id: string): Promise<Doctor | null>;
+  update(doctor: Doctor): Promise<Doctor>;
 }
