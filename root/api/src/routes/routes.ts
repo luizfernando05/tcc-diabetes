5041642fb76239613d53b1c1438fe10a0cdef50a
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-05 11:33:25 -0300

    :sparkles: feat(api-backend): add doctor routes to main routing file

--- root/api/src/routes/routes.ts
+++ root/api/src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { adminRoutes } from './Admin/admin.routes';
+import { doctorRoutes } from './Doctor/doctor.routes';
 
 const routes = Router();
 
 routes.use('/admins', adminRoutes);
+routes.use('doctors', doctorRoutes);
 
 export { routes };
