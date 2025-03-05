12d3da744e75b9eff13978c6cea974c289cbbc37
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-05 11:01:05 -0300

    :sparkles: feat(api-backend): add findByEmail method to IDoctorRepository for retrieving doctors by email

--- root/api/src/domain/repositories/IDoctorRepository.ts
+++ root/api/src/domain/repositories/IDoctorRepository.ts
@@ -2,4 +2,5 @@ import Doctor from '@domain/entities/Doctors';
 
 export interface IDoctorRepository {
   create(doctor: Doctor): Promise<Doctor>;
+  findByEmail(email: string): Promise<Doctor | null>;
 }
