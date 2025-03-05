9661aa2e8e3daf316751462cd16f9a52537dd9f7
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-05 11:01:51 -0300

    :sparkles: feat(api-backend): add findAll method to IDoctorRepository for retrieving all doctors

--- root/api/src/domain/repositories/IDoctorRepository.ts
+++ root/api/src/domain/repositories/IDoctorRepository.ts
@@ -3,4 +3,5 @@ import Doctor from '@domain/entities/Doctors';
 export interface IDoctorRepository {
   create(doctor: Doctor): Promise<Doctor>;
   findByEmail(email: string): Promise<Doctor | null>;
+  findAll(): Promise<Doctor[]>;
 }
