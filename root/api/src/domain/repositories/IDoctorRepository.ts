f9ea1b0130305eb47a3bf16d9cc6bdbb384faae4
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-05 11:02:30 -0300

    :sparkles: feat(api-backend): add findById method to IDoctorRepository for retrieving a doctor by ID

--- root/api/src/domain/repositories/IDoctorRepository.ts
+++ root/api/src/domain/repositories/IDoctorRepository.ts
@@ -4,4 +4,5 @@ export interface IDoctorRepository {
   create(doctor: Doctor): Promise<Doctor>;
   findByEmail(email: string): Promise<Doctor | null>;
   findAll(): Promise<Doctor[]>;
+  findById(id: string): Promise<Doctor | null>;
 }
