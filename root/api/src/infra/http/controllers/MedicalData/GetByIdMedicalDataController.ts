62ebebb26abe4b03febb311b1b291e9523ac6ad5
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:26:42 -0300

    :sparkles: feat(api-backend): add validation for medical data ID in GetByIdMedicalDataController

--- root/api/src/infra/http/controllers/MedicalData/GetByIdMedicalDataController.ts
+++ root/api/src/infra/http/controllers/MedicalData/GetByIdMedicalDataController.ts
@@ -1,6 +1,8 @@
 import GetByIdMedicalDataUseCase from '@application/MedicalData/use-case/GetByIdMedicalDataUseCase';
 import MedicalDataRepository from '@infra/database/repositories/MedicalDataRepository';
+import { AppError } from '@presentation/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
+import * as yup from 'yup';
 
 export class GetByIdMedicalDataController {
   async handle(
@@ -10,6 +12,10 @@ export class GetByIdMedicalDataController {
   ): Promise<Response | void> {
     try {
       const { id } = req.params;
+
+      if (!id || !yup.string().uuid().isValidSync(id)) {
+        throw new AppError('Invalid ID.', 400);
+      }
 
       const medicalDataRepository = new MedicalDataRepository();
       const getByIdMedicalDataUseCase = new GetByIdMedicalDataUseCase(
