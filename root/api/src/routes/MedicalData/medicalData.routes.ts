4aa274fed02e425d55f7c430c6fe5248825b6caf
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:32:37 -0300

    :sparkles: feat(api-backend): add route for updating medical data in medicalDataRoutes

--- root/api/src/routes/MedicalData/medicalData.routes.ts
+++ root/api/src/routes/MedicalData/medicalData.routes.ts
@@ -1,10 +1,12 @@
 import CreateMedicalDataController from '@infra/http/controllers/MedicalData/CreateMedicalDataController';
 import GetByIdMedicalDataController from '@infra/http/controllers/MedicalData/GetByIdMedicalDataController';
+import UpdateMedicalDataController from '@infra/http/controllers/MedicalData/UpdateMedicalDataController';
 import { Router } from 'express';
 
 const medicalDataRoutes = Router();
 const createMedicalDataController = new CreateMedicalDataController();
 const getByIdMedicalDataController = new GetByIdMedicalDataController();
+const updateMedicalDataController = new UpdateMedicalDataController();
 
 medicalDataRoutes.post('/', (req, res, next) => {
   createMedicalDataController.handle(req, res, next);
@@ -12,6 +14,10 @@ medicalDataRoutes.post('/', (req, res, next) => {
 
 medicalDataRoutes.get('/:id', (req, res, next) => {
   getByIdMedicalDataController.handle(req, res, next);
+});
+
+medicalDataRoutes.put('/:id', (req, res, next) => {
+  updateMedicalDataController.handle(req, res, next);
 });
 
 export { medicalDataRoutes };
