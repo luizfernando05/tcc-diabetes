e873752bc11181d76f46659b00ca5a14ab88ea96
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:00:33 -0300

    :sparkles: feat(api-backend): add findByPatientId method to IMedicalDataRepository for retrieving medical data by patient ID

--- root/api/src/domain/repositories/IMedicalDataRepository.ts
+++ root/api/src/domain/repositories/IMedicalDataRepository.ts
@@ -3,4 +3,5 @@ import MedicalData from '@domain/entities/MedicalData';
 export interface IMedicalDataRepository {
   create(medicalData: MedicalData): Promise<MedicalData>;
   findById(id: string): Promise<MedicalData | null>;
+  findByPatientId(patientId: string): Promise<MedicalData | null>;
 }
