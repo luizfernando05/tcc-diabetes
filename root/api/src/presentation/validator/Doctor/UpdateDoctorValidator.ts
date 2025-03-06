b6acfd8e09323cb8e2635cc9733bb21e1fbb3cfa
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 10:39:25 -0300

    :sparkles: feat(api-backend): add UpdateDoctorValidator for validating doctor update inputs

---
+++ root/api/src/presentation/validator/Doctor/UpdateDoctorValidator.ts
@@ -0,0 +1,7 @@
+import * as yup from 'yup';
+
+export const UpdateDoctorValidator = yup.object().shape({
+  name: yup.string().min(3, 'Name must be at least 3 characters long.'),
+  email: yup.string().email('Invalid email format.'),
+  password: yup.string().min(6, 'Password must be at least 6 characters long.'),
+});
