35fd72665340c00626e4efad91c26ca915a90396
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 11:45:06 -0300

    :sparkles: feat(api-backend): add login route for patient authentication

--- root/api/src/routes/Patients/patients.routes.ts
+++ root/api/src/routes/Patients/patients.routes.ts
@@ -1,13 +1,19 @@
 import CreatePatientController from '@infra/http/controllers/Patient/CreatePatientController';
 import GetByIdPatientController from '@infra/http/controllers/Patient/GetByIdPatientController';
+import LoginPatientController from '@infra/http/controllers/Patient/LoginPatientController';
 import { Router } from 'express';
 
 const patientsRoutes = Router();
 const createPatientController = new CreatePatientController();
+const loginPatientController = new LoginPatientController();
 const getByIdPatientController = new GetByIdPatientController();
 
 patientsRoutes.post('/', (req, res, next) => {
   createPatientController.handle(req, res, next);
+});
+
+patientsRoutes.post('/login', (req, res, next) => {
+  loginPatientController.handle(req, res, next);
 });
 
 patientsRoutes.get('/:id', (req, res, next) => {
