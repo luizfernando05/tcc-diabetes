b7df911a08904b80d999c2dc1051b7c841bdd63b
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 11:52:30 -0300

    :sparkles: feat(api-backend): add route for listing all patients

--- root/api/src/routes/Patients/patients.routes.ts
+++ root/api/src/routes/Patients/patients.routes.ts
@@ -1,5 +1,6 @@
 import CreatePatientController from '@infra/http/controllers/Patient/CreatePatientController';
 import GetByIdPatientController from '@infra/http/controllers/Patient/GetByIdPatientController';
+import ListPatientController from '@infra/http/controllers/Patient/ListPatientController';
 import LoginPatientController from '@infra/http/controllers/Patient/LoginPatientController';
 import { Router } from 'express';
 
@@ -7,6 +8,7 @@ const patientsRoutes = Router();
 const createPatientController = new CreatePatientController();
 const loginPatientController = new LoginPatientController();
 const getByIdPatientController = new GetByIdPatientController();
+const listPatientController = new ListPatientController();
 
 patientsRoutes.post('/', (req, res, next) => {
   createPatientController.handle(req, res, next);
@@ -18,6 +20,10 @@ patientsRoutes.post('/login', (req, res, next) => {
 
 patientsRoutes.get('/:id', (req, res, next) => {
   getByIdPatientController.handle(req, res, next);
+});
+
+patientsRoutes.get('/', (req, res, next) => {
+  listPatientController.handle(req, res, next);
 });
 
 export { patientsRoutes };
