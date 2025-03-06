ea06b2cea049fec729e8eb5f44be3cb28bb56381
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:22:58 -0300

    :sparkles: feat(api-backend): add medicalDataRoutes to handle medical data requests

--- root/api/src/routes/routes.ts
+++ root/api/src/routes/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { adminRoutes } from './Admin/admin.routes';
 import { doctorRoutes } from './Doctor/doctor.routes';
 import { patientsRoutes } from './Patients/patients.routes';
+import { medicalDataRoutes } from './MedicalData/medicalData.routes';
 
 const routes = Router();
 
 routes.use('/admins', adminRoutes);
 routes.use('/doctors', doctorRoutes);
 routes.use('/patients', patientsRoutes);
+routes.use('/medical/data', medicalDataRoutes);
 
 export { routes };
