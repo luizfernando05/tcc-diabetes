330b4f4831082534e8e99a25db43617e4bf41f24
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:09:51 -0300

    :sparkles: feat(api-backend): add CreateMedicalDataDTO for creating medical data entries

---
+++ root/api/src/application/MedicalData/dto/CreateMedicalDataDTO.ts
@@ -0,0 +1,13 @@
+export interface CreateMedicalDataDTO {
+  patientId: string;
+  age: number;
+  urea: number;
+  cr: number;
+  hba1c: number;
+  chol: number;
+  tg: number;
+  hdl: number;
+  ldl: number;
+  vldl: number;
+  bmi: number;
+}
