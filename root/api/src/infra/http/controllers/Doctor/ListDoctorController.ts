0aaaa56fedf0d5041a9ff71736389386257567ee
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 10:21:26 -0300

    :sparkles: feat(api-backend): add ListDoctorController for retrieving all doctors

---
+++ root/api/src/infra/http/controllers/Doctor/ListDoctorController.ts
@@ -0,0 +1,24 @@
+import ListDoctorUseCase from '@application/Doctor/use-case/ListDoctorUseCase';
+import DoctorRepository from '@infra/database/repositories/DoctorRepository';
+import { NextFunction, Request, Response } from 'express';
+
+export class ListDoctorController {
+  async hanndle(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    try {
+      const doctorRepository = new DoctorRepository();
+      const lisDoctorUseCase = new ListDoctorUseCase(doctorRepository);
+
+      const doctors = await lisDoctorUseCase.execute();
+
+      return res.status(200).json(doctors);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default ListDoctorController;
