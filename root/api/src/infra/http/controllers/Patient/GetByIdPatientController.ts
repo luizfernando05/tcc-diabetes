ab6d3a356843bc2d6e171be91e1ba30ab9bb11e9
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 11:27:03 -0300

    :sparkles: feat(api-backend): add GetByIdPatientController for retrieving patient details by ID

---
+++ root/api/src/infra/http/controllers/Patient/GetByIdPatientController.ts
@@ -0,0 +1,34 @@
+import GetByIdPatientUseCase from '@application/Patient/use-case/GetByIdPatientUseCase';
+import PatientRepository from '@infra/database/repositories/PatientRepository';
+import { AppError } from '@presentation/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import * as yup from 'yup';
+
+export class GetByIdPatientController {
+  async handle(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    try {
+      const { id } = req.params;
+
+      if (!id || !yup.string().uuid().isValidSync(id)) {
+        throw new AppError('Invalid ID.', 400);
+      }
+
+      const patientRepository = new PatientRepository();
+      const getByIdPatientUseCase = new GetByIdPatientUseCase(
+        patientRepository
+      );
+
+      const patient = await getByIdPatientUseCase.execute(id);
+
+      return res.status(200).json(patient);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default GetByIdPatientController;
