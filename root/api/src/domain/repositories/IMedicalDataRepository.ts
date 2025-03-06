b0c627f858998ca9a85a8bfe823ee5e062297591
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:01:03 -0300

    :sparkles: feat(api-backend): add update method to IMedicalDataRepository for updating medical data

--- root/api/src/domain/repositories/IMedicalDataRepository.ts
+++ root/api/src/domain/repositories/IMedicalDataRepository.ts
@@ -4,4 +4,5 @@ export interface IMedicalDataRepository {
   create(medicalData: MedicalData): Promise<MedicalData>;
   findById(id: string): Promise<MedicalData | null>;
   findByPatientId(patientId: string): Promise<MedicalData | null>;
+  update(medicalData: MedicalData): Promise<MedicalData>;
 }
