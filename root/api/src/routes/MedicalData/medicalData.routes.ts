454143288e9bc7bf84bca08219bf8ec289551f31
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 13:28:08 -0300

    :sparkles: feat(api-backend): add route for retrieving medical data by ID in medicalDataRoutes

--- root/api/src/routes/MedicalData/medicalData.routes.ts
+++ root/api/src/routes/MedicalData/medicalData.routes.ts
@@ -1,11 +1,17 @@
 import CreateMedicalDataController from '@infra/http/controllers/MedicalData/CreateMedicalDataController';
+import GetByIdMedicalDataController from '@infra/http/controllers/MedicalData/GetByIdMedicalDataController';
 import { Router } from 'express';
 
 const medicalDataRoutes = Router();
 const createMedicalDataController = new CreateMedicalDataController();
+const getByIdMedicalDataController = new GetByIdMedicalDataController();
 
 medicalDataRoutes.post('/', (req, res, next) => {
   createMedicalDataController.handle(req, res, next);
+});
+
+medicalDataRoutes.get('/:id', (req, res, next) => {
+  getByIdMedicalDataController.handle(req, res, next);
 });
 
 export { medicalDataRoutes };
