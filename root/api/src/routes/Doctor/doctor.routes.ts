a66169dc2c6b35b3f4f157627b4dbf6f0626abb2
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 10:24:09 -0300

    :sparkles: feat(api-backend): add route for retrieving all doctors

--- root/api/src/routes/Doctor/doctor.routes.ts
+++ root/api/src/routes/Doctor/doctor.routes.ts
@@ -1,5 +1,6 @@
 import CreateDoctorController from '@infra/http/controllers/Doctor/CreateDoctorController';
 import GetByDoctorIdController from '@infra/http/controllers/Doctor/GetByIdDoctorController';
+import ListDoctorController from '@infra/http/controllers/Doctor/ListDoctorController';
 import LoginDoctorController from '@infra/http/controllers/Doctor/LoginDoctorController';
 import { Router } from 'express';
 
@@ -7,6 +8,7 @@ const doctorRoutes = Router();
 const createDoctorController = new CreateDoctorController();
 const loginDoctorController = new LoginDoctorController();
 const getByIdDoctorController = new GetByDoctorIdController();
+const listDoctorController = new ListDoctorController();
 
 doctorRoutes.post('/', (req, res, next) => {
   createDoctorController.handle(req, res, next);
@@ -18,6 +20,10 @@ doctorRoutes.post('/login', (req, res, next) => {
 
 doctorRoutes.get('/:id', (req, res, next) => {
   getByIdDoctorController.handle(req, res, next);
+});
+
+doctorRoutes.get('/', (req, res, next) => {
+  listDoctorController.handle(req, res, next);
 });
 
 export { doctorRoutes };
