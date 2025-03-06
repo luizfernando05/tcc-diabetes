f9c7b0ea85d9df8abd0e8f7904d2ac554c7b9203
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 11:28:31 -0300

    :sparkles: feat(api-backend): add route for retrieving patient details by ID

--- root/api/src/routes/Patients/patients.routes.ts
+++ root/api/src/routes/Patients/patients.routes.ts
@@ -1,11 +1,17 @@
 import CreatePatientController from '@infra/http/controllers/Patient/CreatePatientController';
+import GetByIdPatientController from '@infra/http/controllers/Patient/GetByIdPatientController';
 import { Router } from 'express';
 
 const patientsRoutes = Router();
 const createPatientController = new CreatePatientController();
+const getByIdPatientController = new GetByIdPatientController();
 
 patientsRoutes.post('/', (req, res, next) => {
   createPatientController.handle(req, res, next);
+});
+
+patientsRoutes.get('/:id', (req, res, next) => {
+  getByIdPatientController.handle(req, res, next);
 });
 
 export { patientsRoutes };
