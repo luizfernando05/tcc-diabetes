24df1cf9a2db09a76bcc2740afd8732b210fcd0f
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-06 10:41:27 -0300

    :sparkles: feat(api-backend): add route for updating doctor information

--- root/api/src/routes/Doctor/doctor.routes.ts
+++ root/api/src/routes/Doctor/doctor.routes.ts
@@ -2,6 +2,7 @@ import CreateDoctorController from '@infra/http/controllers/Doctor/CreateDoctorC
 import GetByDoctorIdController from '@infra/http/controllers/Doctor/GetByIdDoctorController';
 import ListDoctorController from '@infra/http/controllers/Doctor/ListDoctorController';
 import LoginDoctorController from '@infra/http/controllers/Doctor/LoginDoctorController';
+import UpdateDoctorController from '@infra/http/controllers/Doctor/UpdateDoctorController';
 import { Router } from 'express';
 
 const doctorRoutes = Router();
@@ -9,6 +10,7 @@ const createDoctorController = new CreateDoctorController();
 const loginDoctorController = new LoginDoctorController();
 const getByIdDoctorController = new GetByDoctorIdController();
 const listDoctorController = new ListDoctorController();
+const updateDoctorController = new UpdateDoctorController();
 
 doctorRoutes.post('/', (req, res, next) => {
   createDoctorController.handle(req, res, next);
@@ -24,6 +26,10 @@ doctorRoutes.get('/:id', (req, res, next) => {
 
 doctorRoutes.get('/', (req, res, next) => {
   listDoctorController.handle(req, res, next);
+});
+
+doctorRoutes.put('/:id', (req, res, next) => {
+  updateDoctorController.handle(req, res, next);
 });
 
 export { doctorRoutes };
