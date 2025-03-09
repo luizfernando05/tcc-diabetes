b4d49cbbe160dd04968cad04d0fd0ffa7ab2613b
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-09 11:32:07 -0300

    :recycle: refactor(api-backend): move authorization check for updating a patient to the correct position

--- root/api/src/infra/http/controllers/Patient/UpdatePatientController.ts
+++ root/api/src/infra/http/controllers/Patient/UpdatePatientController.ts
@@ -17,12 +17,12 @@ export class UpdatePatientController {
       const { id } = req.params;
       const { name, email, password, birthDate, gender } = req.body;
 
-      if (req.user.role !== 'patient' || req.user.id !== id) {
-        throw new AppError('You are not allowed to delete this patient.', 403);
-      }
-
       if (!id || !yup.string().uuid().isValidSync(id)) {
         throw new AppError('Invalid ID.', 400);
+      }
+
+      if (req.user.role !== 'patient' || req.user.id !== id) {
+        throw new AppError('You are not allowed to delete this patient.', 403);
       }
 
       const patientRepository = new PatientRepository();
