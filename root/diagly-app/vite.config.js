a2227bcb81ce63bc6709d30e5312249f364739d5
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-11 10:09:21 -0300

    fix(diagly-app): correct Tailwind CSS import in Vite configuration

--- root/diagly-app/vite.config.js
+++ root/diagly-app/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [react(), svgr(), tailwind()],
 });
