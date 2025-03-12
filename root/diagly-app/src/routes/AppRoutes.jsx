cc7e122c6f8938cd8363a3301ddff96da5a1833b
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-12 13:04:50 -0300

    feat(diagly-app): update routing to navigate to LoginPage based on user type

--- root/diagly-app/src/routes/AppRoutes.jsx
+++ root/diagly-app/src/routes/AppRoutes.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/patients/login" />} />
+        <Route path="/:userType/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
