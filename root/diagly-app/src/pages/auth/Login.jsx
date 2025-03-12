e70eb8d941086d6c329283491e5acf88adfb0870
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-03-12 12:58:00 -0300

    feat(diagly-app): add LogoIcon component and update Login page layout

--- root/diagly-app/src/pages/auth/Login.jsx
+++ root/diagly-app/src/pages/auth/Login.jsx
@@ -1,58 +1,45 @@
-import { useState } from 'react';
-import { login } from '../../services/authService';
+import LogoIcon from '../../assets/logoIcon.svg?react';
+import InputField from '../../components/Forms/InputField';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleLogin = async (role) => {
-    setError('');
-    try {
-      await login(role, email, password);
-      alert('Login successful!');
-    } catch (err) {
-      setError(err);
-    }
-  };
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-xl font-poppins font-bold mb-4">Login</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="mb-2 border p-2 rounded"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="mb-2 border p-2 rounded"
-      />
-      {error && <p className="text-red-500">{error}</p>}
-      <div className="flex gap-2 mt-2">
-        <button
-          onClick={() => handleLogin('admins')}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Admin
-        </button>
-        <button
-          onClick={() => handleLogin('doctors')}
-          className="bg-green-500 text-white px-4 py-2 rounded"
-        >
-          Médico
-        </button>
-        <button
-          onClick={() => handleLogin('patients')}
-          className="bg-purple-500 text-white px-4 py-2 rounded"
-        >
-          Paciente
-        </button>
+    <div className="flex items-center justify-center min-h-screen bg-gray-02">
+      <div className="bg-gray-01 p-8 rounded-lg shadow-lg w-96">
+        <div className="login-header flex justify-center mb-5">
+          <LogoIcon />
+        </div>
+        <h1 className="font-poppins text-gray-12 text-2xl font-semibold text-center mb-2">
+          Welcome back!
+        </h1>
+        <p className="font-roboto text-gray-11 text-sm text-center mb-8">
+          Please enter your details to sing in.
+        </p>
+        <form action="">
+          <InputField
+            label="Email"
+            type="text"
+            name="username"
+            placeholder="Enter your email..."
+          />
+          <InputField label="Password" type="password" name="password" />
+
+          <div className="flex justify-between items-center text-sm mb-4">
+            <div className="flex">
+              <input className="mr-1" type="checkbox" />
+              <p>Remeber me</p>
+            </div>
+            <a className="text-gray-11 hover:underline" href="#">
+              Forgot password?
+            </a>
+          </div>
+
+          <button
+            type="submit"
+            className="font-poppins text-base font-medium w-full bg-yellow-09 text-yellow-12 py-2 rounded-lg hover:bg-yellow-08 transition cursor-pointer"
+          >
+            Sing in
+          </button>
+        </form>
       </div>
     </div>
   );
