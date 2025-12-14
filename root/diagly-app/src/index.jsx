e1d00bac1c5cffcd9520474e900fd16e0139834f
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-12-14 15:08:09 -0300

    :wrench: chore: inserting initial electron configuration

--- root/diagly-app/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import React from 'react';
-import App from './App';
-import './styles.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
